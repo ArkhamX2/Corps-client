76b656655e92cd8a52b9f3589b5d07462ab2cf45
vadimyt <pvcs@yandex.ru>
2024-06-12 14:21:45 +0300

    host to network

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { readFileSync } from 'fs'
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: readFileSync("hostIp.txt").toString()
+  },
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+        input: {
+            main: './index.html',
+        },
+    },
+},
 })
